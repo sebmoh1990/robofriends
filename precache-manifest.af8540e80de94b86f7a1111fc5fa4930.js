cc84bb330c573501cffc3c7de16ce7bb04059104
sebmoh1990 <159629866+sebmoh1990@users.noreply.github.com>
2024-03-21 16:40:39 +0100

    Updates

---
+++ precache-manifest.af8540e80de94b86f7a1111fc5fa4930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02bc94a9d1558221c4931f633b77333",
+    "url": "/sebmoh1990/robofriends.git/index.html"
+  },
+  {
+    "revision": "163e6452bd58740f4447",
+    "url": "/sebmoh1990/robofriends.git/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "283a14ce6dc6a463085f",
+    "url": "/sebmoh1990/robofriends.git/static/css/main.752e827d.chunk.css"
+  },
+  {
+    "revision": "163e6452bd58740f4447",
+    "url": "/sebmoh1990/robofriends.git/static/js/2.aac0fe50.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/sebmoh1990/robofriends.git/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "283a14ce6dc6a463085f",
+    "url": "/sebmoh1990/robofriends.git/static/js/main.72db6a8d.chunk.js"
+  },
+  {
+    "revision": "70435fa87e288151614b",
+    "url": "/sebmoh1990/robofriends.git/static/js/runtime~main.3d89bc16.js"
+  }
+]);
